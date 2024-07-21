a140dad1b9cf7975a010c59d6b4aab1de52c2044
VINAYOMKAR9 <vinayomkar9@gmail.com>
2024-07-21 17:58:19 +0530

    react

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+const TodoList =({todo})=>{
+    return (
+        <div>
+        {
+            todo.map((el)=>{
+                return (
+                <div key={el.id}>
+                    <h4>{el.title}</h4>
+                    <p>{el.status}</p>
+                    <p>{el.status?'completed':'Active'}</p>
+                    </div>
+            )})}
+    </div>
+    )
+}
+
+
 const Todo = ()=>{
 const [text,setText]= React.useState('')
-const[todo,setTodo]= React.useState([])
+const[todo,setTodo]= React.useState([{id:1,title:'Todo 1', status:false}])
 
 const handleChange=(e)=>{
     let val = e.target.value;
@@ -8,15 +25,28 @@ const handleChange=(e)=>{
     
 }
 const handleClick=()=>{
-    console.log(text)
+    const obj={
+        id: todo.length+1,
+        title:text,
+        status:false
 
+    }
+    setTodo([...todo,obj])
+    setText('')
 }
+console.log(todo)
 
     return (
         <div>
             <h1>TOdo</h1>
-            <input type='text' placeholder='Enter todo' onChange={handleChange}/>>
+            <input type='text' placeholder='Enter todo' onChange={handleChange}/>
         <button onClick={handleClick}>Add</button>
+        
+       <div>
+        <TodoList todo={todo} />
+       </div>
+        
+        
         </div>
     )
 }
