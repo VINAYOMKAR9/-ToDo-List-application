3d5197b204d1698ebfade5bfebc5be240cc15a40
VINAYOMKAR9 <vinayomkar9@gmail.com>
2024-07-22 16:08:00 +0530

    react

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const TodoList =({todo})=>{
+const TodoList =({todo,handleDelete,handleCheck})=>{
     return (
-        <div>
+        <div className='showDiv'> 
         {
             todo.map((el)=>{
                 return (
-                <div key={el.id}>
+                <div className='inDiv' key={el.id} style={{backgroundColor:el.checkBox?'lightgreen':'lightGray'}}>
                     <h4>{el.title}</h4>
-                    <p>{el.status}</p>
-                    <p>{el.status?'completed':'Active'}</p>
+                    <p>{el.checkBox}</p>
+                   <lable>Done :
+                    <input type='checkbox' onChange={()=>handleCheck(el.id)} />
+                   </lable>
+                    {/* <button o={()=>handleDelete(el.id)}>Delete</button> */}
+                    <button  onClick={()=>handleDelete(el.id)}>Delete</button>
                     </div>
             )})}
     </div>
@@ -17,7 +21,7 @@ const TodoList =({todo})=>{
 
 const Todo = ()=>{
 const [text,setText]= React.useState('')
-const[todo,setTodo]= React.useState([{id:1,title:'Todo 1', status:false}])
+const[todo,setTodo]= React.useState([{id:1,title:'Todo 1', checkBox:false},])
 
 const handleChange=(e)=>{
     let val = e.target.value;
@@ -28,22 +32,49 @@ const handleClick=()=>{
     const obj={
         id: todo.length+1,
         title:text,
-        status:false
+        checkBox:false
 
     }
     setTodo([...todo,obj])
     setText('')
 }
-console.log(todo)
+
+// const handleTo =()=>{
+// }
+
+const handleDelete=(id)=>{
+    const filteredData = todo.filter((el)=>el.id != id); 
+    setTodo(filteredData)
+}
+
+const handleCheck=(id)=>{
+    const checkB=todo.map((el)=>
+        el.id == id ? { ...el,checkBox :!el.checkBox}: el 
+ );
+ setTodo(checkB)
+
+    // if(id==false){
+    //     id=true
+    //     console.log('tr')
+    //     setTodo(id)
+    // }
+    // else{
+    //     id= false
+    //     console.log('fal')
+    //     // setTodo(id)
+    // }
+
+}
+// console.log(todo) 
 
     return (
         <div>
-            <h1>TOdo</h1>
+            <h1>Todo</h1>
             <input type='text' placeholder='Enter todo' onChange={handleChange}/>
         <button onClick={handleClick}>Add</button>
         
        <div>
-        <TodoList todo={todo} />
+        <TodoList todo={todo} handleDelete={handleDelete} handleCheck={handleCheck} />
        </div>
         
         
